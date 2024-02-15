313502a31a3b1a7b40436afd450d2b2804c7546d
Riji Jacob <82180314+riji-teck@users.noreply.github.com>
2024-02-15 10:02:38 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ToDoList] (
+    [Id] INT NOT NULL IDENTITY PRIMARY KEY,
+    [StartDate] DATE NULL,
+    [EndDate]   DATE NULL,
+    [IsActive]  BIT  NULL,
+    [Content] NVARCHAR(MAX) NULL
+);
---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE ToDoList;
+
+CREATE TABLE [dbo].[ToDoList] (
+    [Id]      INT  NOT NULL IDENTITY(1,1),
+    [Content] TEXT NOT NULL,
+    CONSTRAINT [PK_ToDoList] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+SET IDENTITY_INSERT [dbo].[ToDoList].[Id] ON
+go
