3f90acf2b7b24fddca4b678fe62951d9393ff15f
Riji Jacob <82180314+riji-teck@users.noreply.github.com>
2024-02-15 10:07:51 -0500

    Add files via upload

---
+++ ToDoList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList
+{
+    public class ToDoList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime? StartDate { get; set; }
+
+        [Required]
+        public DateTime? EndDate { get; set; }
+
+        [Required]
+        public bool? IsActive { get; set; }
+
+        public bool? Star { get; set; }
+
+    }
+}
