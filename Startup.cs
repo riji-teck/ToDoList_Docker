811aaf4e8824eb9a63fa0f32b66a335ce0671ed9
Riji Jacob <82180314+riji-teck@users.noreply.github.com>
2024-02-15 10:05:20 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ToDoList.Infrastructure;
+
+namespace ToDoList
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ToDoContext")));
+
+            //services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString(@"Server=(localdb)\mssqllocaldb;Database=ToDoList")));
+
+            //using var context = new ApplicationDbContext(contextOptions);
+            //IServiceCollection serviceCollection = services.AddDbContext<ToDoContext>(options => options.UseSqlite("Filename=ToDoDB.db"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=ToDo}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
